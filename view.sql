be0c80f13442d6ef689a87b32dba839b880cee35
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-17 17:38:23 +0900

    drop view and practice create

--- view.sql
+++ view.sql
@@ -17,3 +17,18 @@
 -- 	where t.to_date >= NOW();
 -- 
 -- SELECT * FROM test_view WHERE emp_no <= 300000;
+
+-- DROP VIEW test_view;
+
+-- CREATE VIEW now_dept
+-- AS
+-- 	SELECT e.emp_no, concat(e.last_name, ' ' ,e.first_name) AS Full_name, d.dept_name
+-- 	FROM employees e
+-- 		INNER JOIN dept_emp de
+-- 			ON e.emp_no = de.emp_no
+-- 		INNER JOIN departments d
+-- 			ON de.dept_no = d.dept_no
+-- 	WHERE de.to_date >= NOW()
+-- 	ORDER BY e.emp_no;
+	
+SELECT * FROM now_dept;
