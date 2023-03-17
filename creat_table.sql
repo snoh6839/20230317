835ffce3d927cf30a9014db3097b241294dc4529
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-17 13:09:44 +0900

    customer table

--- creat_table.sql
+++ creat_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE customers (
     cus_no INT(5)
-	 ,customer_name VARCHAR(50)
-	 ,customer_age INT(3)
-	 ,customer_gender CHAR(1)
+	 ,customer_name VARCHAR(50) NOT NULL
+	 ,customer_age INT(3) NOT NULL
+-- 	 ,customer_gender CHAR(1)
+	 ,customer_gender ENUM('F','M')
+	 ,signup_date DATE NOT NULL
+	 ,withdrawal_date DATE
     ,CONSTRAINT pk_customers PRIMARY KEY (cus_no)
 );
+
